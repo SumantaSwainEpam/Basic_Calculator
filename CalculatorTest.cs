d8be3ee0b4fa03978b6ee4f3cc0c48026272833b
sumanta_swain@epam.com <sumanta_swain@epam.com>
2025-05-06 20:13:42 +0530

    Did some Code changes

--- CalculatorTest.cs
+++ CalculatorTest.cs
@@ -77,5 +77,76 @@ namespace Basic_Calculator
             Assert.Throws<DivideByZeroException>(() => _calculator.Divide(a, b));
         }
 
+        [Test]
+        public void Add_NegativeAndPositiveNumber_ReturnsCorrectSum()
+        {
+            // Arrange
+            int a = -5;
+            int b = 3;
+
+            // Act
+            double result = _calculator.Add(a, b);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(-2));
+        }
+
+        [Test]
+        public void Subtract_LargerFromSmallerNumber_ReturnsNegativeDifference()
+        {
+            // Arrange
+            int a = 3;
+            int b = 5;
+
+            // Act
+            double result = _calculator.Subtract(a, b);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(-2));
+        }
+
+        [Test]
+        public void Multiply_ByZero_ReturnsZero()
+        {
+            // Arrange
+            int a = 5;
+            int b = 0;
+
+            // Act
+            double result = _calculator.Multiply(a, b);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Divide_NegativeByPositiveNumber_ReturnsNegativeQuotient()
+        {
+            // Arrange
+            int a = -6;
+            int b = 3;
+
+            // Act
+            double result = _calculator.Divide(a, b);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(-2));
+        }
+
+        [Test]
+        public void Divide_TwoNegativeNumbers_ReturnsPositiveQuotient()
+        {
+            // Arrange
+            int a = -6;
+            int b = -3;
+
+            // Act
+            double result = _calculator.Divide(a, b);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+
     }
 }
